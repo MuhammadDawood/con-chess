75eab89557181aab4c0d259e8b924a568446b1ba
poptarts4liffe <poptarts4liffe@cc721601-98a6-0dc3-8ec4-373cfa12f250>
2010-03-20 17:12:58 +0000

    Cleaned up the interface for each class.

--- trunk/include/board.h
+++ trunk/include/board.h
@@ -1,6 +1,6 @@
 /****************************************************************************************
 * File: board.h                                                                         *
-* Authors: Stuart Larsen(SCL)                                                           *
+* Authors: Stuart Larsen(SCL), Carl Vitula (CEV)                                        *
 * Description:                                                                          *
 * 	Board is a member of class Game. It controls all the pieces on the board. If    *
 *	the user trys to move a piece Board handles the actual changing of positioning  *
@@ -8,21 +8,26 @@
 *                                                                                       *      
 * Major Modifications                                                                   *
 * 19Mar10 SCL	Intial Coding.                                                          *
+* 19Mar10 CEV	Changed Piece vectors.                                                  *
+* 20Mar10 SCL	Cleaned up class.                                                       * 
 ****************************************************************************************/
 class Board
 {
-     vector<piece> whitePieces(16); //Vectors should be fine, since we don't have to add anything and will have to index a lot.
-     vector<piece> blackPieces(16); //I'm in favor of splitting it up though.
-     void initiate(); /* Set up stuff */
-     Moves *moves;
+
+	vector<Piece> myWhitePieces(16); //Vectors should be fine, since we don't have to add anything and will have to index a lot.
+     	vector<Piece> myBlackPieces(16); //I'm in favor of splitting it up though.
      
-     public:
-	  Board() {/* Set up and draw board, add pieces */}
+     	/* Will make all the pieces and add them into the vectors */
+	void initiateBoard();
+
+public:
+	Board() {/* Set up and draw board, add pieces */}
 	  
-	  drawBoard();
-	  getPiece(int x, int y);
-	  move();
-	  void drawAll() { for_each(myPieces.begin(), myPieces.end(), mem_fun(&Piece::draw));
-	  void promotePiece()
-	  /* vector<Pair<int, int>>? */ avalibleMoves() { /* Takes possible moves from Piece::possibleMoves(), and figures out with ones are avalible, no other piece there, or if it would cause reveal check */ }
+	void drawBoard();
+	Piece& getPiece(int x, int y);
+	Piece& getPiece(Move move);
+	void movePiece(Move move);
+	void drawAll() { for_each(myPieces.begin(), myPieces.end(), mem_fun(&Piece::draw));
+	void promotePiece();
+	vector<Move> avalibleMoves() { /* Takes possible moves from Piece::possibleMoves(), and figures out with ones are avalible, no other piece there, or if it would cause reveal check */ }
 };
--- trunk/include/game.h
+++ trunk/include/game.h
@@ -1,30 +1,34 @@
 /****************************************************************************************
-* File: game.h                                                                         *
-* Authors: Stuart Larsen(SCL)                                                           *
+* File: game.h                                                                          *
+* Authors: Stuart Larsen(SCL), Carl Vitula(CEV)                                         *
 * Description:                                                                          *
 * 	Each individual game will be it's own object. It'll handle the players, board,  *
 *	and all major interactions pertaining to them.                                  *
 *                                                                                       *      
 * Major Modifications                                                                   *
 * 19Mar10 SCL	Intial Coding.                                                          *
+* 19Mar10 CEV	Timer work.                                                             *
+* 20Mar10 SCL	Cleaned up class.                                                       *
 ****************************************************************************************/
 enum Turn { BLACKTURN, WHITETURN };
 
 class Game
 {
-	Board *board;
+	Board board;
 	Player player1, player2;
 	Timer timer1, timer2; //Shouldn't timer be a member of player?
-	Move *moves;
+	vector<Move> moves;
 	
 public:
 	Game(Player one, Player two) : player1(one), player2(two)
-	{ /* Initialize board with all the pieces. Allocate some memory for moves */ }
+	{  }
 
 	int loadGameFromFile(string fileName);
 	int saveGame(string fileName);
 	
 	/* It'll have the game loop where it ask each player for their move, and then sends the stuff to board */
 	void playGame();
+	
+	/* Adds move to the moves vector */
 	void addMove(Move move);
 }
